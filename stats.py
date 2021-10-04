25a2caa392a3bffdb4249960e8a1a71e6d12929b
tboonma <tboonma@hotmail.com>
2021-10-04 14:44:09 +0700

    Fix code and improve code quality

--- stats.py
+++ stats.py
@@ -1,6 +1,10 @@
+"""Module for variance and standard deviation."""
+from math import sqrt
+
+
 def variance(data):
     """Returns the population variance of a list (array) of numbers in data.
-    
+
     The variance is the sum of squared differences between data values
     and their mean, divided by the number of items in the list (n).
     This is different from the Python library function statistics.variance
@@ -9,9 +13,9 @@ def variance(data):
     Example: variance([1,5]) is ((1-3)**2 + (5-3)**2)/2 = 4.
 
     (These are Google style comments for arguments, returns, and exceptions.)
-    
+
     Args:
-        data: list of numbers for which variance will be computed. 
+        data: list of numbers for which variance will be computed.
            Must contain at least one element.
     Returns:
         population variance of values in data list.
@@ -28,13 +32,13 @@ def variance(data):
     4.0
     """
     # some deliberately misformatted code. Use flake8 to fix.
-    n=len(data)  
-    if n==0:
-        raise ValueError( )
-    average = sum(data)/n
-    return sum( [(x-average)**2 for x in data] )/n
+    length = len(data)
+    if length == 0:
+        raise ValueError("Must have at least one value.")
+    average = sum(data)/length
+    return sum([(x-average)**2 for x in data])/length
+
 
 def stdev(data):
     """The population standard deviation of a list of data values."""
     return sqrt(variance(data))
-
